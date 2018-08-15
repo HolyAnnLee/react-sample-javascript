014c03d1bc1540d877d3e48ef333db171499c667
Dante <634344393@qq.com>
2018-08-16 00:17:18 +0800

    update: modify package.json for the formater

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './app.css';
+import Layout from './pages/Layout';
 
 class App extends Component {
   render() {
     return (
-      <div className="hello-world">
-        hello world
-        <img alt="logo" src="./assets/img/logo.svg" width="120px" />
+      <div>
+        <div className="hello-world">
+          hello world
+          <img alt="logo" src="./assets/img/logo.svg" width="120px" />
+        </div>
+        <Layout />
       </div>
     );
   }
